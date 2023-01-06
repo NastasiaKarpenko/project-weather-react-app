7c721471352e9ddea9a922d45aa3df1cfe14da0d
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2023-01-06 22:35:14 +0000

    added css for Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Weather.css";
+import "bootstrap/dist/css/bootstrap.css";
+
 
 export default function Weather(){
     return(
@@ -7,7 +9,7 @@ export default function Weather(){
     <div className="row">
         <div claasName="col-10">
             <div className="row">
-                <div className="col-8 justify-content space-evenly">
+                <div className="col-8 cities">
                     <a href="/" data-value="Irpin">Irpin</a>
                     <a href="/" data-value="Krakow">Krakow</a>
                     <a href="/" data-value="Eindhoven">Eindhoven</a>
@@ -15,40 +17,56 @@ export default function Weather(){
                     <a href="/" data-value="Drogheda">Drogheda</a>
                 </div>
                 <div className="col-2">
-                    <button className="btn btn-outline-warning">
+                    <button className="btn btn-outline-warning shadow-sm w-100">
                         Current
                     </button>
 
                 </div>
             </div>
             <div className="row">
-                <div className="col-8">
-                   <input type="search" placeholder="Type a city..." />
+                <div className="col-8 mt-2">
+                   <input type="search" 
+                   placeholder="Type a city..." 
+                    className="form-control shadow-sm"
+                    autoFocus="on"/>
                 </div>
                 <div className="col-2">
-                    <button className="btn btn-warning">
+                    <button className="btn btn-warning w-100 mt-2 shadow-sm">
                         Search
                     </button>
                 </div>
             </div>
             <div className="row">
                 <h2>Kyiv</h2>
-                <p>Last updated: Mon, 15:30</p>
+                <p>Last updated: <span className="current-date">Mon, 15:30</span></p>
             </div>
             <div className="row">
                  <div className="col-4">
-                     <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"></img>
-                    11°C |°F
-
+                     <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" />
+                     <strong className="temperature">-3</strong>1
+                     <span className="units">
+              <a href="/" id="celsius-link">°C</a> |
+              <a href="/" id="fahrenheit-link">°F</a>
+            </span>
                  </div>
                  <div className="col-4">
-        
+                 <ul>
+                     <li>Clouds</li>
+                     <li>Humidity: 42%</li>
+                     <li>Wind: 2 km/h</li>
+                </ul>
+
                  </div>
             </div>
 
         </div>
+    
         <div claasName="col-2">
-            
+            <div className="row">
+                <div className="col">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"/> Thu 18°12°
+                </div>
+            </div>
         </div>
     </div>
 </div>
